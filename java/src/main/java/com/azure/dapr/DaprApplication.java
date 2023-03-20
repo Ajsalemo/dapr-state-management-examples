2aa8caef08528aa2ad05edaacc629a628a1b0d81
Anthony Salemo <ajssalemo@gmail.com>
2023-03-20 19:15:56 -0400

    starting with on a java example

---
+++ java/src/main/java/com/azure/dapr/DaprApplication.java
@@ -0,0 +1,13 @@
+package com.azure.dapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaprApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/azure/dapr/DaprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.dapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
