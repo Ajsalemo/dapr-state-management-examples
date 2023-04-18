9a293bc1e90a6b0a17008c63f02085b3df1922f8
Anthony Salemo <ajssalemo@gmail.com>
2023-04-18 19:40:13 -0400

    able to successfully POST state to the statestore

--- java/sdk/src/main/java/com/azure/dapr/Controllers/CreateStateController.java
+++ java/sdk/src/main/java/com/azure/dapr/Controllers/CreateStateController.java
@@ -2,7 +2,8 @@ package com.azure.dapr.Controllers;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.azure.dapr.Entities.Order.Order;
 import com.azure.dapr.Entities.State.State;
 import com.azure.dapr.Services.DaprService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class CreateStateController {
     @PostMapping("/order/create")
-    public String createState(@RequestBody Order order) throws JsonProcessingException {
+    public ResponseEntity<String> createState(@RequestBody Order order) throws Exception {
         UUID uuid = UUID.randomUUID();
 
         order.setData(order.data);
@@ -25,9 +25,8 @@ public class CreateStateController {
 
         State[] payload = new State[] { state };
         ObjectMapper objectMapper = new ObjectMapper();
-
-        DaprService.createState(uuid.toString(), objectMapper.writeValueAsString(payload));
-
-        return "T";
+        // Save state with the DaprService
+        DaprService.createState(uuid.toString(), objectMapper.writeValueAsString(payload)).block();
+        return new ResponseEntity<String>("Order created with ID: " + uuid, HttpStatus.CREATED);
     }
 }
