4cf6f6c931d7151a8971d695acf0091966887d25
Anthony Salemo <ajssalemo@gmail.com>
2023-05-02 19:53:39 -0400

    getting closer to contructiong/recontructing the shape of the req-body for dapr

--- go/http/controllers/createState/createStateController.go
+++ go/http/controllers/createState/createStateController.go
@@ -3,6 +3,8 @@ package createState
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -10,8 +12,35 @@ import (
 
 func CreateStateController(w http.ResponseWriter, r *http.Request) {
 	uuid := uuid.New()
-	msg := fmt.Sprintf("Created order with Order ID: %v", uuid)
-	m := map[string]string{"msg": msg}
+	// msg := fmt.Sprintf("Created order with Order ID: %v", uuid)
+
+	// res, err := http.NewRequest(http.MethodPost, "http://localhost:3500/v1.0/state/statestore")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	type order struct {
+		Key string `json:"key"`
+		Value string `json:"value"`
+	}
+
+	requestBody, err := ioutil.ReadAll(r.Body)
+	s := string(requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	state := []order{
+		{
+			Key: uuid.String(),
+			Value: s,
+		},
+	}
+	
+	o, _ := json.Marshal(state)
+	fmt.Println(string(o))
+
+	m := map[string]string{"msg": "T"}
 	e := json.NewEncoder(w)
 	e.Encode(m)
 }
