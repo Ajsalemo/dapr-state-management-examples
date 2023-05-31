fb60900c8376b736736e0dbb56c93b3445d3927d
Anthony Salemo <ajssalemo@gmail.com>
2023-05-30 23:05:01 -0400

    containerized the php/http example

--- php/http/app/Http/Controllers/OrderCreateController.php
+++ php/http/app/Http/Controllers/OrderCreateController.php
@@ -1,20 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 class OrderCreateController extends Controller
 {
-    public function orderCreate(Request $request) {
+    public function orderCreate(Request $request)
+    {
         $uuid = Str::uuid();
         $res = $request->all();
-        $value = array('key' => $uuid, 'value' => $res);
 
-        $structuredArray = array();
-        array_push($structuredArray, $value);
-        
-        $json = json_encode($structuredArray);
-        return $json;
+        $structuredArr = array();
+        $value = array('key' => $uuid, 'value' => $res);
+        array_push($structuredArr, $value);
+
+        // Make a POST request to the Dapr sidecar
+        $d = Http::post('http://localhost:3500/v1.0/state/statestore', $structuredArr);
+        $msg = "Order created with ID: " . $uuid;
+
+        return $msg;
     }
 }
