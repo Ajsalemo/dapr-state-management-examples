03f8ba58cd07a86c278761919ca0ce1fc28ce167
Anthony Salemo <ajssalemo@gmail.com>
2023-06-06 21:23:45 -0400

    fixed namespacing issues with laravel classes and imports

--- php/sdk/app/Dapr/Dapr.php
+++ php/sdk/app/Dapr/Dapr.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Library;
+namespace App\Dapr;
 
 use Dapr\Client\DaprClient;
 
--- php/sdk/app/Http/Controllers/OrderCreateController.php
+++ php/sdk/app/Http/Controllers/OrderCreateController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Library\Dapr;
+use App\Dapr\Dapr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
